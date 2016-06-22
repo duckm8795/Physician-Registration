8f80ff727285707fc562986e42d72725bb27c20e
duckieu8795 <duc.kieu8795@gmail.com>
2016-06-22 17:29:58 +0700

    -  [Onboard-2] Create account
    Create the layout of the create account screen

--- app/src/main/java/com/trainingandroidpart1/physicianregistration/MainActivity.java
+++ app/src/main/java/com/trainingandroidpart1/physicianregistration/MainActivity.java
@@ -1,8 +1,10 @@
 package com.trainingandroidpart1.physicianregistration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -16,5 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.btn_invitation);
 
+    }
+    public void sign_up( View view){
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
